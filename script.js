51a3a96ff92e8311bd18f0580f7ad9b5a00c954f
Nischal <107695989+Nischal117@users.noreply.github.com>
2022-07-30 09:46:16 +0545

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+
+function toogleMenu(){
+    const navlinks = document.querySelector(".nav-links");
+    navlinks.classList.toggle("mobile-menu")
+}
+
+
+
+
+    let navbar = document.querySelector("nav");
+    var scrollPrev = window.pageYOffset;
+    window.onscroll = function(){
+var scrollCur = window.pageYOffset;
+if (scrollPrev > scrollCur){
+    navbar.style.top = "0"
+}
+else {
+    navbar.style.top = "-90px"
+}
+
+scrollPrev = scrollCur
+    }
